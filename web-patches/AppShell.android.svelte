472dfeca3f9679e549183ab8f01afd18e3e86b66
xpmini-gif <xpmini@gmail.com>
2026-09-26 10:45:46 +0200

    Add web-patches/AppShell.android.svelte

---
+++ web-patches/AppShell.android.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Toolbar from "./Toolbar.svelte";
+  import StatusBar from "./StatusBar.svelte";
+  import PanelShell from "./PanelShell.svelte";
+  import Toast from "$/components/ui/Toast.svelte";
+</script>
+
+<div class="flex h-[100dvh] min-h-0 flex-col overflow-hidden bg-[var(--color-surface-0)]">
+  <Toolbar />
+  <PanelShell />
+  <StatusBar />
+</div>
+
+<Toast />
